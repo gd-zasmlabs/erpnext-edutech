1605623947ad237e8b76d53e79b5ca48d9fc28a0
gd-zasmlabs <gd@zasmlabs.com>
2025-07-22 13:37:31 +0530

    Initial commit of EduTech app

---
+++ edutech/faculty_management/doctype/faculty_assignment/test_faculty_assignment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Glavin and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFacultyAssignment(FrappeTestCase):
+	pass
